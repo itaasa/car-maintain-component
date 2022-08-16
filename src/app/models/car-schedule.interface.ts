be801de40f59105c8385c87371a963e68f6e4c25
itaasa <itaasarvin@gmail.com>
2022-08-16 15:33:40 -0400

    updated model

--- src/app/models/car-schedule.interface.ts
+++ src/app/models/car-schedule.interface.ts
@@ -2,7 +2,7 @@ import { Car } from './car.interface';
 import { Maintenance } from './maintenance.interface';
 
 export interface CarSchedule {
-  id?: string;
+  _id?: string;
   car: Car;
   maintenances: Maintenance[];
 }
